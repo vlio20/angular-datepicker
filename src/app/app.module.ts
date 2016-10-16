5b2b945dc397ace3bb7f361d7c8752ac7aa39a48
Vlad Ioffe <vlio20@gmail.com>
2016-10-17 01:25:10 +0300

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {ObDayPickerComponent} from './ob-day-picker/ob-day-picker.component';
 import {ObCalendarComponent} from './ob-calendar/ob-calendar.component';
@@ -20,5 +19,5 @@ import {ObCalendarComponent} from './ob-calendar/ob-calendar.component';
   bootstrap: [AppComponent]
 })
 
-export class AppModule {
+export class ObDatePickerModule {
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import './polyfills.ts';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { AppModule } from './app/';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {environment} from './environments/environment';
+import {ObDatePickerModule} from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(ObDatePickerModule);
