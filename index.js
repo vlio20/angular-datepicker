5f4a953eaf221982514d4eee41bcef86c4fbba47
Vlad Ioffe <vlio20@gmail.com>
2016-11-05 00:58:06 +0200

    publish try 5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./bin/app/date-picker.module'));
+__export(require('./bin/app/dp-date-picker.module'));
