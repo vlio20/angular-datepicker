f1851346ea0e7b4cf4403aa582ca3680cef78d5c
Vlad Ioffe <vlio20@gmail.com>
2016-11-05 10:18:41 +0200

    fixing bin build

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,7 @@ const tsc = require('gulp-typescript');
 const less = require('less');
 const sourcemaps = require('gulp-sourcemaps');
 const merge = require('merge2');
-const tsProject1 = tsc.createProject('tsconfig.json');
-const tsProject2 = tsc.createProject('tsconfig.json');
-const tsProject3 = tsc.createProject('tsconfig.json');
+const tsProject = tsc.createProject('tsconfig.json');
 
 const pluginOptions = {
   base: './src/app/',
@@ -19,25 +17,17 @@ const pluginOptions = {
 };
 
 gulp.task('build', () => {
-  const src = gulp.src(['./src/**/*.ts', '!./src/**/*.spec.ts']);
-  const dst = gulp.dest('./bin');
-
-  const jsMap1 = src
-    .pipe(tsProject1())
+  const tsResult = gulp.src(['./src/**/*.ts', '!./src/**/*.spec.ts'])
     .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('.'));
-
-  const jsMap2 = src
     .pipe(inlineTemplate(pluginOptions))
-    .pipe(tsProject2())
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('.'));
 
-  const dts = src.pipe(tsProject3());
+    .pipe(tsProject());
 
   return merge([
-    jsMap1.pipe(dst),   // writes .js and .map files
-    jsMap2.pipe(dst),   // writes .js and .map files
-    dts.dts.pipe(dst)  // writes .d.ts files
+    tsResult.js
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./bin')),
+    tsResult.dts
+      .pipe(gulp.dest('./bin'))
   ]);
 });
