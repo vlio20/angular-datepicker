6de4bef3c25eb04613fbe985205b284f4fdd3883
Vlad Ioffe <vlio20@gmail.com>
2017-03-23 21:36:10 +0200

    fixing build

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,20 @@ const less = require('less');
 const sourcemaps = require('gulp-sourcemaps');
 const merge = require('merge2');
 const tsProject = tsc.createProject('tsconfig.json');
+const path = require('path');
+const fs = require('fs');
 
 const pluginOptions = {
   base: './src/app/',
   useRelativePaths: true,
-  styleProcessor: (path, ext, file, cb) => {
-    less.render(file, (e, out) => {
-      console.error(e);
-      process.exit(1);
+  styleProcessor: (src, ext, file, cb) => {
+    less.render(fs.readFileSync(src).toString(), {
+      filename: path.resolve(src)
+    }, (e, out) => {
+      if (e) {
+        console.error(e);
+        process.exit(1);
+      }
       cb(null, out.css);
     });
   }
