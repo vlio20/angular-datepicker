c3a1610fddff326e0518c7317c5bbef8fc81680b
Vlad Ioffe <vlio20@gmail.com>
2017-04-08 11:00:35 +0300

    removing redundant component

--- src/app/dp-date-picker.module.ts
+++ src/app/dp-date-picker.module.ts
@@ -5,7 +5,6 @@ import {DpDayPickerComponent} from './dp-day-picker/dp-day-picker.component';
 import {DpCalendarMonthComponent} from './dp-day-calendar/dp-day-calendar.component';
 import {DpCalendarComponent} from './dp-calendar/dp-calendar.component';
 import {DomHelper} from './common/services/dom-appender/dom-appender.service';
-import {MonthPickerComponent} from './dp-month-picker/month-picker.component';
 import { MonthCalendarComponent } from './dp-month-calendar/dp-month-calendar.component';
 import {UtilsService} from './common/services/utils/utils.service';
 export {DpDayPickerComponent} from './dp-day-picker/dp-day-picker.component'
@@ -21,7 +20,6 @@ export {DpCalendarComponent} from './dp-calendar/dp-calendar.component';
     DpDayPickerComponent,
     DpCalendarMonthComponent,
     DpCalendarComponent,
-    MonthPickerComponent,
     MonthCalendarComponent
   ],
   imports: [
