d2a26ae751e6282cc4983721e24d2aa4692a20f1
Vlad Ioffe <vlio20@gmail.com>
2017-04-21 21:37:33 +0300

    fixing e2e test

--- src/app/demo/demo/demo.component.ts
+++ src/app/demo/demo/demo.component.ts
@@ -12,7 +12,7 @@ import {DayCalendarComponent} from '../../day-calendar/day-calendar.component';
   styleUrls: ['./demo.component.less']
 })
 export class DemoComponent {
-  @ViewChild('dayPicker') dayPicker: DatePickerComponent;
+  @ViewChild('datePicker') datePicker: DatePickerComponent;
   demoFormat = 'DD-MM-YYYY';
   readonly DAYS = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa'];
   pickerMode = 'dayPicker';
@@ -73,11 +73,11 @@ export class DemoComponent {
   }
 
   openCalendar() {
-    this.dayPicker.api.open();
+    this.datePicker.api.open();
   }
 
   closeCalendar() {
-    this.dayPicker.api.close();
+    this.datePicker.api.close();
   }
 
   log(item) {
