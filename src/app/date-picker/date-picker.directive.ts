dbae6aba2336bb56434fe2f1c74d0b5b606a71a1
Vlad Ioffe <vlio20@gmail.com>
2017-05-26 23:16:40 +0300

    fixing required validation for directive

--- src/app/date-picker/date-picker.directive.ts
+++ src/app/date-picker/date-picker.directive.ts
@@ -129,9 +129,11 @@ export class DatePickerDirective implements OnInit {
     });
 
     this.datePicker.registerOnChange((value) => {
-      const errors = this.datePicker.validateFn(value);
       this.formControl.control.setValue(this.datePicker.inputElementValue);
-      this.formControl.control.setErrors(errors);
+      const errors = this.datePicker.validateFn(value);
+      if (errors) {
+        this.formControl.control.setErrors(errors);
+      }
       this.formControl.control.markAsDirty();
     });
   }
