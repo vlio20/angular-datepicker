97cd58fe08c12b5d618e3cc982b67df4d5b31de1
Róbert Kiss <robert.kiss1@ibm.com>
2017-09-26 15:23:09 +0200

    fix(date-picker): Fix OnPush change detection strategy

--- src/app/time-select/time-select.component.ts
+++ src/app/time-select/time-select.component.ts
@@ -3,6 +3,7 @@ import {SingleCalendarValue} from '../common/types/single-calendar-value';
 import {ECalendarMode} from '../common/types/calendar-mode-enum';
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   EventEmitter,
   forwardRef,
@@ -77,6 +78,7 @@ export class TimeSelectComponent implements OnInit, OnChanges, ControlValueAcces
     this._selected = selected;
     this.calculateTimeParts(this.selected);
     this.onChangeCallback(this.processOnChangeCallback(selected));
+    this.cd.markForCheck();
   }
 
   get selected(): Moment {
@@ -88,7 +90,8 @@ export class TimeSelectComponent implements OnInit, OnChanges, ControlValueAcces
   };
 
   constructor(public timeSelectService: TimeSelectService,
-              public utilsService: UtilsService) {
+              public utilsService: UtilsService,
+              private cd: ChangeDetectorRef) {
   }
 
   ngOnInit() {
