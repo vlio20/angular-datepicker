c3c15b9aa78ccade98b3637f2620889f995a08f4
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-05-11 19:31:01 +0200

    Attempt no 2

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,23 +1,19 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const {SpecReporter} = require('jasmine-spec-reporter');
-const args = process.argv;
-const headless = args.indexOf('headless') !== -1;
+const { SpecReporter } = require('jasmine-spec-reporter');
+const headless = process.env.HEADLESS;
 
 exports.config = {
   allScriptsTimeout: 120000,
-  specs: [
-    './e2e/**/*spec.ts'
-  ],
+  specs: ['./e2e/**/*spec.ts'],
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: headless ? [
-        '--headless',
-        '--disable-gpu',
-        '--window-size=1280,1024'] : []
-    }
+      args: headless
+        ? ['--headless', '--disable-gpu', '--window-size=1280,1024']
+        : [],
+    },
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -25,13 +21,13 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 120000,
-    print: function () {
-    }
+    print: function() {},
   },
   onPrepare() {
-    jasmine.getEnv().addReporter(
-      new SpecReporter({spec: {displayStacktrace: true}}));
+    jasmine
+      .getEnv()
+      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
 
     require('ts-node').register({ project: 'e2e/tsconfig.e2e.json' });
-  }
+  },
 };
