d6b3169c04aee577b5b267a7b67225a30e84e9d9
Vlad Ioffe <vlio20@gmail.com>
2018-11-27 21:35:04 +0200

    fixing unit tests + travis fix

--- src/app/date-picker/date-picker-directive.service.spec.ts
+++ src/app/date-picker/date-picker-directive.service.spec.ts
@@ -3,6 +3,8 @@ import {DatePickerDirectiveService} from './date-picker-directive.service';
 import {UtilsService} from '../common/services/utils/utils.service';
 
 describe('Service: DatePickerDirective', () => {
+  const fakeElement: HTMLElement = document.createElement('div');
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [DatePickerDirectiveService, UtilsService]
@@ -11,21 +13,21 @@ describe('Service: DatePickerDirective', () => {
 
   it('should check convertToElement method', inject([DatePickerDirectiveService, UtilsService],
     (service: DatePickerDirectiveService, stubUtilsService: UtilsService) => {
-      stubUtilsService.closestParent = jasmine.createSpy('closestParent').and.returnValue('fakeElement');
+      stubUtilsService.closestParent = jasmine.createSpy('closestParent').and.returnValue(fakeElement);
 
       const baseElement = <any>{};
-      const element1 = service.convertToHTMLElement({nativeElement: 'fakeElement'}, baseElement);
-      expect(element1).toBe('fakeElement');
+      const element1 = service.convertToHTMLElement({nativeElement: fakeElement}, baseElement);
+      expect(element1).toBe(fakeElement);
       expect(stubUtilsService.closestParent).not.toHaveBeenCalled();
 
       const element2 = service.convertToHTMLElement('.notFound', baseElement);
-      expect(element2).toBe('fakeElement');
+      expect(element2).toBe(<HTMLElement>fakeElement);
       expect(stubUtilsService.closestParent).toHaveBeenCalledWith(baseElement, '.notFound');
     }));
 
   it('should check getConfig method', inject([DatePickerDirectiveService],
     (service: DatePickerDirectiveService) => {
-      service.convertToHTMLElement = jasmine.createSpy('convertToHTMLElement').and.returnValue('fakeElement');
+      service.convertToHTMLElement = jasmine.createSpy('convertToHTMLElement').and.returnValue(fakeElement);
 
       const config1 = service.getConfig();
       expect(config1).toEqual({hideInputContainer: true});
@@ -38,7 +40,6 @@ describe('Service: DatePickerDirective', () => {
       });
       expect(service.convertToHTMLElement).not.toHaveBeenCalled();
 
-      const fakeElement = {};
       const config3 = service.getConfig({allowMultiSelect: true}, {nativeElement: fakeElement});
       expect(config3).toEqual({
         allowMultiSelect: true,
@@ -53,7 +54,7 @@ describe('Service: DatePickerDirective', () => {
       expect(config4).toEqual({
         allowMultiSelect: true,
         hideInputContainer: true,
-        inputElementContainer: 'fakeElement'
+        inputElementContainer: fakeElement
       });
       expect(service.convertToHTMLElement).toHaveBeenCalledWith(fakeAttachElementRef, fakeElement);
 
@@ -61,7 +62,7 @@ describe('Service: DatePickerDirective', () => {
       expect(config5).toEqual({
         allowMultiSelect: true,
         hideInputContainer: true,
-        inputElementContainer: 'fakeElement'
+        inputElementContainer: fakeElement
       });
       expect(service.convertToHTMLElement).toHaveBeenCalledWith('someSelector', fakeElement);
     }));
