58acccdb77cd028b7c0a88e5a0fd913e0cb7a017
Vlad Ioffe <vioffe@outbrain.com>
2020-11-19 17:57:41 +0200

    GA added back

--- src/demo/services/ga/ga.service.ts
+++ src/demo/services/ga/ga.service.ts
@@ -1,21 +1,19 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment';
 
-declare const ga: Function;
+declare const gtag: Function;
 
 @Injectable()
 export class GaService {
 
   public emitEvent(eventCategory: string,
-                   eventAction: string,
-                   eventLabel: string = null,
+                   eventLabel: string,
                    eventValue: number = null) {
-    if (environment.production && window['ga']) {
-      ga('send', 'event', {
-        eventCategory: eventCategory,
-        eventLabel: eventLabel,
-        eventAction: eventAction,
-        eventValue: eventValue
+    if (environment.production && window['gtag']) {
+      gtag('event', 'send', {
+        event_category: eventCategory,
+        event_label: eventLabel,
+        value: eventValue
       });
     }
   }
