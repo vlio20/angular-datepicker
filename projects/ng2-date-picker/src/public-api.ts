3d25078fa900bc797d73e57be015e03ec85a647f
Vlad Ioffe <vlio20@gmail.com>
2021-12-22 10:37:30 +0200

    demo working with new stracture

---
+++ projects/ng2-date-picker/src/public-api.ts
@@ -0,0 +1,16 @@
+export {SingleCalendarValue} from './lib/common/types/single-calendar-value';
+export {IDate} from './lib/common/models/date.model';
+export {ECalendarMode} from './lib/common/types/calendar-mode-enum';
+export {IDay} from './lib/day-calendar/day.model';
+export {IMonth} from './lib/month-calendar/month.model';
+export {ECalendarValue} from './lib/common/types/calendar-value-enum';
+export {CalendarValue} from './lib/common/types/calendar-value';
+export {IDayCalendarConfig} from './lib/day-calendar/day-calendar-config.model';
+export {IDayEvent} from './lib/day-calendar/day.model';
+export {IDatePickerConfig} from './lib/date-picker/date-picker-config.model';
+export {IDatePickerDirectiveConfig} from './lib/date-picker/date-picker-directive-config.model';
+export {IMonthCalendarConfig} from './lib/month-calendar/month-calendar-config';
+export {ISelectionEvent} from './lib/common/types/selection-event.model';
+export {SelectEvent} from './lib/common/types/selection-event.enum';
+
+export * from './lib/date-picker.module';
