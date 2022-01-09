eeb9d8722b0d8fcf71e6c14bea86de75d88a5086
Vlad Ioffe <vlio20@gmail.com>
2022-01-09 10:12:31 +0200

    fixing error (#581)

--- projects/ng2-date-picker/src/lib/common/dayjs/dayjs.ref.ts
+++ projects/ng2-date-picker/src/lib/common/dayjs/dayjs.ref.ts
@@ -1,19 +1,14 @@
 import dayjs from 'dayjs';
-import 'dayjs/plugin/isSameOrAfter';
-import 'dayjs/plugin/isSameOrBefore';
-import 'dayjs/plugin/isBetween';
-import 'dayjs/plugin/isoWeek';
-import 'dayjs/plugin/customParseFormat';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isBetween from 'dayjs/plugin/isBetween';
+import isoWeek from 'dayjs/plugin/isoWeek';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
-// @ts-ignore
-dayjs.extend(require('dayjs/plugin/isSameOrAfter'));
-// @ts-ignore
-dayjs.extend(require('dayjs/plugin/isSameOrBefore'));
-// @ts-ignore
-dayjs.extend(require('dayjs/plugin/isBetween'));
-// @ts-ignore
-dayjs.extend(require('dayjs/plugin/isoWeek'));
-// @ts-ignore
-dayjs.extend(require('dayjs/plugin/customParseFormat'));
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isBetween);
+dayjs.extend(isoWeek);
+dayjs.extend(customParseFormat);
 
 export const dayjsRef = dayjs;
