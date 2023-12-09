960f174e832fa8d7dc241352d3f1122516d979cc
Vlad Ioffe <vlio20@gmail.com>
2023-12-09 21:42:55 +0200

    playwrite 12

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:4200',
+    url: 'http://localhost:4200',
     reuseExistingServer: true,
   },
 });
